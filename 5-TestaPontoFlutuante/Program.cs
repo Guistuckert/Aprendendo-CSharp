5f54d114268c78bb43456c1027929d0fc2a7777b
Guistuckert <ghstuckert@gmail.com>
2022-11-26 18:17:18 -0300

    Adicionar arquivos de projeto.

---
+++ 5-TestaPontoFlutuante/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 1250.70;
+            Console.WriteLine("meu salário é " + salario);
+
+        int divisao = 7 / 5;
+        Console.WriteLine(divisao);
+    }
+
+}
